269cb97045e630a14c8f43127e5cb4d8022cf62c
erosgbcs <gabrielagsamosam@gmail.com>
2026-08-11 02:02:48 +0800

    Re-add service worker for PWA

--- frontend/sw.js
+++ frontend/sw.js
@@ -1,20 +1,19 @@
-const CACHE_NAME = 'acadhub-v2';
-const urlsToCache = [
-  '/AcadHub/',
-  '/AcadHub/index.html',
-  '/AcadHub/manifest.json'
-];
-
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open('acadhub-v1').then((cache) => {
+      return cache.addAll([
+        '/AcadHub/',
+        '/AcadHub/index.html',
+        '/AcadHub/manifest.json'
+      ]);
+    })
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
