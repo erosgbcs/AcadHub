0cd52af8b3c306984aad4691a7fc7ff93b4d8042
erosgbcs <gabrielagsamosam@gmail.com>
2026-09-20 13:30:41 +0800

    feat(splash): add animated loading screen with title shine effect

--- script.js
+++ script.js
@@ -76,7 +76,7 @@ let selectedRating = 0;
 let currentTab = 'notes';
 // Splash screen timing
 const splashStartTime = Date.now();
-const SPLASH_MIN_VISIBLE_MS = 3000;
+const SPLASH_MIN_VISIBLE_MS = 3500;
 
 
 let testQuestions = [];
